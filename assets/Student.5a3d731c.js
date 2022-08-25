39e1b81eb4723ac64567f98a7b4ac5b8d54cd8fd
ken0814 <103400040+ken0814@users.noreply.github.com>
2022-08-25 00:52:40 +0000

    Deploying to gh-pages from @ ken0814/gymFun_front@e99765a903e91b8d9d0218744c77e79c9ae48a6f 🚀

---
+++ assets/Student.5a3d731c.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,_ as c}from"./index.d0332747.js";function n(r,a){return e(),t("h1",null,"coachStudent")}const o={},_=c(o,[["render",n]]);export{_ as default};
--- assets/Student.adabab80.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as t,_ as c}from"./index.a4fdb704.js";function n(r,a){return e(),t("h1",null,"coachStudent")}const o={},_=c(o,[["render",n]]);export{_ as default};
